79939b5b7dea59bf07a10d1a8df2326dc20dca70
Dzmitry Navitski <dzmitry.navitski@lemellabs.com>
2022-04-28 20:58:15 +0300

    log admin credentials

--- src/main/java/steps/adminSteps/HomePageSteps.java
+++ src/main/java/steps/adminSteps/HomePageSteps.java
@@ -1,6 +1,9 @@
 package steps.adminSteps;
 
 import net.thucydides.core.steps.ScenarioSteps;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pages.CatalogPage;
 import pages.admin.AdminLoginPage;
 import utils.Config;
 
@@ -8,11 +11,14 @@ public class HomePageSteps extends ScenarioSteps {
 
     private AdminLoginPage homePage;
 
+    private static final Logger logger = LoggerFactory.getLogger(AdminLoginPage.class);
+
+
     public void loginAsAdmin() {
         homePage.openPage();
         homePage.enterLogin(Config.getUsers().getAdmin().getEmail());
-        System.out.println(Config.getUsers().getAdmin().getEmail());
-        System.out.println(Config.getUsers().getAdmin().getPassword());
+        logger.info(Config.getUsers().getAdmin().getEmail());
+        logger.info(Config.getUsers().getAdmin().getPassword());
         homePage.enterPassword(Config.getUsers().getAdmin().getPassword());
         homePage.clickLoginBtn();
         homePage.waitForLogin();
